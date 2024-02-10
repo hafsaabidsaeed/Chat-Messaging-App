93bacec0eb01ecc79221f3c3b74c074e269abc6a
hafsaabidsaeed <128362965+hafsaabidsaeed@users.noreply.github.com>
2024-02-10 17:34:03 +0500

    Login Screen UI
    Added animations in the login screen.

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat_messaging_app/constants/colors.dart';
+import 'package:chat_messaging_app/screens/home_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -12,43 +13,68 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _isAnimate = false;
+  @override
+  void initState(){
+    super.initState();
+    Future.delayed(Duration(milliseconds: 500 ), () {
+      setState(() {
+        _isAnimate = true;
+      });
+    } );
+  }
+  
   @override
   Widget build(BuildContext context) {
+    //initializing media query
     mq = MediaQuery.of(context).size;
 
     return Scaffold(
       appBar: AppBar(
-        leading: const Icon(CupertinoIcons.home),
         title: const Text('Welcome to Conversa'),
       ),
       body: Stack(
         children: [
-          Positioned(
+          AnimatedPositioned(
               top: mq.height * 0.15,
-              left: mq.width * 0.25,
+              right: _isAnimate ? mq.width*0.25 : -mq.width*0.5 ,
               width: mq.width * 0.5,
+              duration: const Duration(seconds: 1),
               child: Image.asset('assets/images/messages.png')),
           Positioned(
             bottom: mq.height * 0.15,
             left: mq.width * 0.075,
-            width: mq.width * 0.85,
+             width: mq.width * 0.85,
             height: mq.height * 0.08,
             child: ElevatedButton.icon(
               style: ElevatedButton.styleFrom(
-                backgroundColor: lightPink,
+                backgroundColor: CupertinoColors.lightBackgroundGray ,
                 shape: const StadiumBorder(),
                 elevation: 1,
               ),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const HomeScreen()) );
+              },
               icon: Image.asset('assets/images/google.png', height:mq.height*0.05 ,),
-              label: const Text(
-                'Sign in with Google',
+              label: RichText(
+              text:const TextSpan(
                 style: TextStyle(
                     fontWeight: FontWeight.w500,
                     fontSize: 20,
-                    color: Color(0xFF241468)),
+                    color: Color(0xFF241468)
+                ),
+                children: [
+                  TextSpan(
+                    text: 'Login with '
+                  ),
+                  TextSpan(
+                    text: 'Google',
+                    style:  TextStyle(fontWeight: FontWeight.w500)
+                  )
+                ]
               ),
             ),
+          ),
           ),
         ],
       ),
